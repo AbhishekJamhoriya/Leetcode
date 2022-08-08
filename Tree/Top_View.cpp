6ff4771cf388fc0f78d17dcc2bae15cebf762370
AbhishekJamhoriya <62591179+AbhishekJamhoriya@users.noreply.github.com>
2022-08-08 23:56:32 +0530

    Create Top_View.cpp

---
+++ Tree/Top_View.cpp
@@ -0,0 +1,30 @@
+vector<int> topView(Node *root)
+    {
+        vector<int>ans;
+        if(!root){
+            return ans;
+        }
+        map<int,int>mp;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            Node *temp=q.front().first;
+            int i=q.front().second;
+            q.pop();
+            if(mp.find(i)==mp.end()){
+                mp[i]=temp->data; 
+            }
+           
+            if(temp->left){
+                q.push({temp->left,i-1});
+            }
+            if(temp->right){
+                q.push({temp->right,i+1});
+            }
+            
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
