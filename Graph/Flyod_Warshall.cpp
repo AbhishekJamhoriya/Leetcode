43cebecda0c8ff3e4139787c7c3508122281b855
AbhishekJamhoriya <62591179+AbhishekJamhoriya@users.noreply.github.com>
2022-08-28 02:43:17 +0530

    Create Flyod_Warshall.cpp

---
+++ Graph/Flyod_Warshall.cpp
@@ -0,0 +1,26 @@
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n=matrix.size();
+	    int inf=1e9;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j]==-1){
+	                matrix[i][j]=inf;
+	            }
+	        }
+	    };
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            for(int k=0;k<n;k++){
+	                matrix[j][k]=min(matrix[j][k],matrix[j][i]+matrix[i][k]);  
+	                
+	            }
+	        }
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j]==inf){
+	                matrix[i][j]=-1;
+	            }
+	        }
+	    }
+	}
