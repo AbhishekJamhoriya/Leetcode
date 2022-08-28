716e35ccb734f55f07e7cad0f136a7109defae8c
AbhishekJamhoriya <62591179+AbhishekJamhoriya@users.noreply.github.com>
2022-08-29 03:01:34 +0530

    Create Bellman_ford.cpp

---
+++ Graph/Bellman_ford.cpp
@@ -0,0 +1,19 @@
+	public:
+	/*  Function to implement Dijkstra
+    *   adj: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
+        vector<int>dis(V,1e8);
+        dis[S]=0;
+        for(int i=0;i<V;i++){
+            for(auto it:adj){
+                int u=it[0];
+                int v=it[1];
+                int w=it[2];
+                dis[v]=min(dis[v],dis[u]+w);
+            }
+        }
+        return dis;
+    }
