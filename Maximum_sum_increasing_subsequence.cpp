8f8ae987eb56f11f835093bb3a9df8963ac42bd8
AbhishekJamhoriya <62591179+AbhishekJamhoriya@users.noreply.github.com>
2022-09-09 13:09:21 +0530

    Create Maximum_sum_increasing_subsequence.cpp

---
+++ Maximum_sum_increasing_subsequence.cpp
@@ -0,0 +1,26 @@
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    int ans=0;
+	    vector<int>val(n,0);
+	    
+	    ans=arr[0];
+	    for(int i=0;i<n;i++){
+	        val[i]=arr[i];
+	    }
+	    for(int i=1;i<n;i++){
+	       
+	        for(int j=i-1;j>=0;j--){
+	            
+	            if(arr[i]>arr[j]){
+	                val[i]=max(val[i],arr[i]+val[j]);
+	                ans=max(ans,val[i]);
+	               // break;
+	            }
+	        }
+	       // cout<<val[i]<<" ";
+	    }
+	    return ans;
+	}  
