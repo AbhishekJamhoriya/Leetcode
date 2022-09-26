e33135386e5d0170dd6615b88c710724ec48cffb
AbhishekJamhoriya <62591179+AbhishekJamhoriya@users.noreply.github.com>
2022-09-27 00:37:00 +0530

    Update Maximum_sum_increasing_subsequence.cpp

--- DP/Maximum_sum_increasing_subsequence.cpp
+++ DP/Maximum_sum_increasing_subsequence.cpp
@@ -1,30 +1,25 @@
-class Solution {
-public:
-    int dp[501][501];
-    int solve(string a,string b,int i,int j){
-        if(i<0){
-            return j+1;
-        }
-        if(j<0){
-            return i+1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(a[i]==b[j]){
-            return dp[i][j]=solve(a,b,i-1,j-1);
-        }
-        int p=solve(a,b,i,j-1);
-        int q=solve(a,b,i-1,j);
-        int r=solve(a,b,i-1,j-1);
-        // cout<<p<<" "<<q<<" "<<r<<" "<<i<<" "<<j<< endl;
-        return dp[i][j]=1+min({p,q,r});
-    }
-    int minDistance(string word1, string word2) {
-        int n=word1.size()-1;
-        int m=word2.size()-1;
-        memset(dp,-1,sizeof(dp));
-        return solve(word1,word2,n,m);
-        
-    }
-};
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    int ans=0;
+	    vector<int>val(n,0);
+	    
+	    ans=arr[0];
+	    for(int i=0;i<n;i++){
+	        val[i]=arr[i];
+	    }
+	    for(int i=1;i<n;i++){
+	       
+	        for(int j=i-1;j>=0;j--){
+	            
+	            if(arr[i]>arr[j]){
+	                val[i]=max(val[i],arr[i]+val[j]);
+	                ans=max(ans,val[i]);
+	               // break;
+	            }
+	        }
+	       // cout<<val[i]<<" ";
+	    }
+	    return ans;
+	}  
